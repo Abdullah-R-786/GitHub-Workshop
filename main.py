c7c764d5d32d711c6a265dca95000f82b65fdeea
Abdullah <KRMYHyperion@gmail.com>
2026-08-27 16:12:06 +1000

    update

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ pass
 #when CHANGES section shows up, commit the change by setting a message and pressing commit. 
 
 #I have made changes 
+
+
+#add an existing Github repository to your local machine using the command 'git clone <repo link>' or 'git clone <repo link> <folder name>' to specify a folder name.
+
+
+#git remote add origin <repo link> to add a remote repository to your local repository.
+# then do git push -u origin main
+#Your local git repository is now linked to the remote repository on GitHub. You can now push and pull changes between your local and remote repositories.
