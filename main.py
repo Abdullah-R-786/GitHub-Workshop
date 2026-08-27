65290325051e3279c04c734049f10d75bebd639d
Abdullah <KRMYHyperion@gmail.com>
2026-08-27 16:04:17 +1000

    revert

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ pass
 
 # add the file via the command line using the command 'git add <filename>'  or git add . Also add the file extension e.g. .py, .txt, .md, etc. to add all files in the current directory. After adding, the 'U' will change to 'A' indicating that the file is now staged for commit.
 
-#when CHANGES section shows up, commit the change by setting a message and pressing commit. The 'A' will change to 'M' indicating that the file is now modified.
+#when CHANGES section shows up, commit the change by setting a message and pressing commit. 
+
+#I have made changes 
